61a3013d330bd01f26685ef6e69ae74a94e447e7
valgehall@gmail.com <Cooper1808+>
2021-09-20 16:02:09 +0300

    added maven

---
+++ src/main/java/com/geoleo/kaardid/KaardidApplication.java
@@ -0,0 +1,13 @@
+package com.geoleo.kaardid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaardidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaardidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/geoleo/kaardid/KaardidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geoleo.kaardid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaardidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
